1549427e634d5ef942dad7aa6b244ccf34973f7e
pavelks <korsun4@mail.ru>
2021-06-19 11:56:05 +0300

    0.1.3

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,10 @@ const { src, dest } = require('gulp');
 
 function buildTask() {
     return src([
-        'core/**/*',
         'package.json',
         'README.md',
         'LICENSE'
-    ]).pipe(dest('node_modules/@hardjs'));
+    ]).pipe(dest('core'));
 }
 
 exports.build = buildTask;
